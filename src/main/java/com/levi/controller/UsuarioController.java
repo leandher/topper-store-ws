4da972f220ecb7c6739ea6cd00a1091299a4cb5a
Unknown <leandher_bessa@hotmail.com>
2017-12-06 22:16:25 -0300

    Tratar conflito no front

--- src/main/java/com/levi/controller/UsuarioController.java
+++ src/main/java/com/levi/controller/UsuarioController.java
@@ -25,54 +25,56 @@ import com.levi.service.UsuarioService;
 @RequestMapping("/api/usuarios")
 @Controller
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Usuario> getLogin(@RequestBody Usuario user) {
-		Usuario usuario = usuarioService.login(user.getEmail(), user.getPassword());
-		
-		if(usuario != null){
+		Usuario usuario = usuarioService.login(user.getEmail(),
+				user.getPassword());
+
+		if (usuario != null) {
 			return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 		} else {
 			return new ResponseEntity<Usuario>(HttpStatus.BAD_REQUEST);
 		}
 	}
-	
-	@RequestMapping(value ="/", produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Usuario> getAllUsuarios() {
 		return usuarioService.getUsuarios();
 	}
-	
+
 	@RequestMapping(value = "/{idUsuario}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Usuario getUsuario(@PathVariable Integer idUsuario) {
 		return usuarioService.getUsuario(idUsuario);
 	}
-	
-	@RequestMapping(method=RequestMethod.POST, value="/", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+
+	@RequestMapping(method = RequestMethod.POST, value = "/", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Usuario> addUsuario(@RequestBody Usuario usuario) {
 		usuario.setUserRole(Role.USER.toString());
-		try{
-			usuarioService.addUsuario(usuario);	
+		try {
+			usuarioService.addUsuario(usuario);
 			return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
-		} catch(RollbackException e){
-			return new ResponseEntity<Usuario>(HttpStatus.CONFLICT);
-		} 
+		}  catch(Exception e){
+			return new ResponseEntity<Usuario>(new Usuario(), HttpStatus.CONFLICT);
+		}
 	}
-	
-	@RequestMapping(method=RequestMethod.PUT, value="/{idUsuario}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Usuario> updateUsuario(@RequestBody Usuario usuario, @PathVariable Integer idUsuario) {
-		
-		try{
-			usuarioService.updateUsuario(usuario);	
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/{idUsuario}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Usuario> updateUsuario(@RequestBody Usuario usuario,
+			@PathVariable Integer idUsuario) {
+
+		try {
+			usuarioService.updateUsuario(usuario);
 			return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
-		} catch(RollbackException e){
+		} catch (RollbackException e) {
 			return new ResponseEntity<Usuario>(HttpStatus.CONFLICT);
 		}
 	}
-	
-	@RequestMapping(method=RequestMethod.DELETE,value="/{idUsuario}")
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/{idUsuario}")
 	public ResponseEntity<Usuario> deleteUsuario(@PathVariable Integer idUsuario) {
 		usuarioService.deleteUsuario(idUsuario);
 		return new ResponseEntity<Usuario>(HttpStatus.OK);
